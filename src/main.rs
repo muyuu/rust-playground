6a898eab4a205fa31dd798d85bb01c9c564e699c
muyuu <anticyborg@gmail.com>
2022-01-27 09:47:32 +0900

    use gcd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 mod algorithm;
 
-fn main() {}
+fn main() {
+    let arr = [42, 28];
+    println!(
+        "the value of GCD {}, and {} is {}.",
+        arr[0],
+        arr[1],
+        algorithm::gcd::gcd(arr[0], arr[1])
+    );
+}
